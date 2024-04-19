786210a0fa2f0f9ab2c0a225dddc0ae378d3efb8
felipe-coletti <felipecoletti2@gmail.com>
2024-04-19 09:44:20 -0300

    Finalização da página de empréstimos

--- BookSystemFront/src/pages/Borrowings/index.jsx
+++ BookSystemFront/src/pages/Borrowings/index.jsx
@@ -6,7 +6,18 @@ import Button from '../../components/global/Button'
 const Borrowings = () => {
     const data = [
         {
-            name: ""
+            name: "Jackeline Menezes",
+            phoneNumber: "(11) 98822-8833",
+            borrowingDate: "21/04/2024",
+            returnDate: "21/05/2024",
+            status: 1
+        },
+        {
+            name: "Yudi Tamashiro",
+            phoneNumber: "(11) 4002-8922",
+            borrowingDate: "21/04/2024",
+            returnDate: "21/05/2024",
+            status: 0
         }
     ]
 
@@ -33,7 +44,7 @@ const Borrowings = () => {
                         <div className={styles.buttonsArea}>
                             <input className={styles.input} placeholder='Pesquisar'/>
                             <Button>
-
+                                <span class="material-symbols-outlined">search</span>
                             </Button>
                         </div>
                         <div className={styles.buttonsArea}>
@@ -46,6 +57,19 @@ const Borrowings = () => {
                         {data.map((item, i) => (
                             <div className={styles.cardContainer} key={i}>
                                 <p className={styles.paragraph}>{item.name}</p>
+                                <p className={styles.paragraph}>{item.phoneNumber}</p>
+                                <div className={styles.textArea}>
+                                    <h2 className={styles.secondaryTitle}>Data de retirada</h2>
+                                    <p className={styles.paragraph}>{item.borrowingDate}</p>
+                                </div>
+                                <div className={styles.textArea}>
+                                    <h2 className={styles.secondaryTitle}>Data de devolução</h2>
+                                    <p className={styles.paragraph}>{item.returnDate}</p>
+                                </div>
+                                {item.status == 0 && <div className={styles.pendingStatus}>Em andamento</div>}
+                                {item.status == 1 && <div className={styles.returnedStatus}>Devolvido</div>}
+                                {item.status == 2 && <div className={styles.notReturnedStatus}>Não devolvido</div>}
+                                <Button>Ver detalhes</Button>
                             </div>
                         ))}
                     </div>
