06250d54d775bb5b519f8420d14456c31ffd1f19
felipe-coletti <felipecoletti2@gmail.com>
2024-04-24 20:48:42 -0300

    Select | Tradução dos termos para português brasileiro

--- BookSystemFront/src/components/global/Select/index.jsx
+++ BookSystemFront/src/components/global/Select/index.jsx
@@ -2,9 +2,9 @@ export default function Select(props) {
     return (
         <select>
             <option value="" selected disabled>{props.selected}</option>
-            {props.options.map((option, i)=> {
+            {props.opcoes.map((opcao, i)=> {
                 return (
-                    <option key={i} value={option.value}>{option.text}</option>
+                    <option key={i} value={opcao.valor}>{opcao.texto}</option>
                 )
             })}
         </select>
