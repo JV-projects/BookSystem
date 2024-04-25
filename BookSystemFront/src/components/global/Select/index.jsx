c795455d5e483d2b0cb5969af333a131cd39b4d2
felipe-coletti <felipecoletti2@gmail.com>
2024-04-25 15:23:20 -0300

    Select | Aplicação de estilo e personalização

--- BookSystemFront/src/components/global/Select/index.jsx
+++ BookSystemFront/src/components/global/Select/index.jsx
@@ -1,12 +1,19 @@
+import styles from './styles.module.css'
+
 export default function Select(props) {
     return (
-        <select>
-            <option value="" selected disabled>{props.selected}</option>
-            {props.opcoes.map((opcao, i)=> {
-                return (
-                    <option key={i} value={opcao.valor}>{opcao.texto}</option>
-                )
-            })}
-        </select>
+        <div className={styles.container}>
+            <select className={styles.select}>
+                <option value="" selected disabled>{props.selected}</option>
+                {props.opcoes.map((opcao, i)=> {
+                    return (
+                        <option key={i} value={opcao.valor}>{opcao.texto}</option>
+                    )
+                })}
+            </select>
+            <div className={styles.icon}>
+                <span className="material-symbols-outlined">expand_more</span>
+            </div>
+        </div>
     )
 }
