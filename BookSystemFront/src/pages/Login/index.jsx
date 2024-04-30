18d57b40f63e0ff960d348803c58e9b6502b5b12
Carjooj <victor@4info.com.br>
2024-04-29 23:37:57 -0300

    Arrumando navegações no login e no novoEmprestimo, corrigindo estilo de botão link

--- BookSystemFront/src/pages/Login/index.jsx
+++ BookSystemFront/src/pages/Login/index.jsx
@@ -1,6 +1,8 @@
 import styles from './styles.module.css'
 import Input from '../../components/global/Input'
 import Button from '../../components/global/Button'
+import { Link } from "react-router-dom"
+
 
 export default function Login() {
     return (
@@ -10,16 +12,18 @@ export default function Login() {
                 <form className={styles.formulario}>
                     <label className={styles.rotulo}>
                         E-mail
-                        <Input type="text" required/>
+                        <Input type="text" />
                     </label>
                     <label className={styles.rotulo}>
                         Senha
-                        <Input type="password" required/>
+                        <Input type="password" />
                     </label>
                     <div>
                         <a className={styles.link} href="">Esqueceu a senha?</a>
                     </div>
-                    <Button tipoBotao="primario" type="submit">Entrar</Button>
+                    <Button tipoBotao="primario" type="submit">
+                        <Link to="/gerenciamento">Login</Link>
+                    </Button>
                 </form>
             </div>
         </div>
