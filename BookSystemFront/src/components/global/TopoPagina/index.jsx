7ddfc0d66e9ed603248ee681f924bb0361edfa3a
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-05-04 21:36:52 -0300

    TopoPagina | Criando renderização condicional para links

--- BookSystemFront/src/components/global/TopoPagina/index.jsx
+++ BookSystemFront/src/components/global/TopoPagina/index.jsx
@@ -2,15 +2,14 @@ import Subtitulo from "../Subtitulo";
 import Titulo from "../Titulo";
 import Button from "../Button";
 import styles from "./styles.module.css"
+import { Link } from "react-router-dom";
 
 export default function TopoPagina({ titulo, subtitulo, link }) {
 
     return (
         <>
             <div className={styles.container_topo}>
-                <a href={link ? link : "#"}>
-                    <Button icone="arrow_back" tipoBotao="botaoVoltar" />
-                </a>
+               <ButtonLink link={link} />
                 <div className={styles.headings}>
                     <Titulo titulo={titulo} />
                     <Subtitulo subtitulo={subtitulo} />
@@ -18,4 +17,15 @@ export default function TopoPagina({ titulo, subtitulo, link }) {
             </div>
         </>
     )
+}
+
+function ButtonLink({link}){
+    if(link){
+        return(
+            <Link to={link}>
+                 <Button icone="arrow_back" tipoBotao="botaoVoltar" />
+            </Link>
+           
+        )
+    }
 }
