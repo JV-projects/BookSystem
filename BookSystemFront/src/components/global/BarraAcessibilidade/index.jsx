177a359ddc73bc92d26b64337ee9042dcc327429
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-05-05 20:13:09 -0300

    BarraAcessibilidade | Refatorando itens
    Co-authored-by: Victor Cardoso Andrade <Carjooj@users.noreply.github.com>

--- BookSystemFront/src/components/global/BarraAcessibilidade/index.jsx
+++ BookSystemFront/src/components/global/BarraAcessibilidade/index.jsx
@@ -79,11 +79,6 @@ export default function BarraAcessibilidade() {
     return (
         <div className={styles.container}>
             <ul className={styles.lista}>
-                <li><a className={styles.link} href="#conteudo" accessKey="1" title="ir para o conteúdo">Ir para o conteúdo [1]</a></li>
-                <li><a className={styles.link} href="#menu" accessKey="2" title="ir para o menu">Ir para o menu [2]</a></li>
-                <li><a className={styles.link} href="#rodape" accessKey="3" title="ir para o rodapé">Ir para o rodapé [3]</a></li>
-            </ul>
-            <ul className={styles.lista}>
                 <li><Link className={styles.link} to="/acessibilidade">Acessibilidade</Link></li>
                 <li><button className={styles.botao} onClick={handleContraste}>Contraste</button></li>
                 <li><button className={styles.botao} onClick={handleSemContraste}>Sem contraste</button></li>
