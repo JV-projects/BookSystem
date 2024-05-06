3a1cc8cc3e61a4eba7674c90cd77b6c850be8a25
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-05-06 19:25:37 -0300

    BarraAcessibilidade | Alto contraste e fonte (+ e -) refatorados

--- BookSystemFront/src/components/global/BarraAcessibilidade/index.jsx
+++ BookSystemFront/src/components/global/BarraAcessibilidade/index.jsx
@@ -1,76 +1,49 @@
 import styles from "./styles.module.css"
 import { Link } from "react-router-dom"
-import { CSSGlobalVariables } from "css-global-variables"
-import { cssVariaveis } from "../../../../public/js/acessibilidade.js"
-import { fonte } from "../../../../public/js/acessibilidade.js"
-import { setFonte } from "../../../../public/js/acessibilidade.js"
+import Cookies from 'js-cookie'
+import { useEffect } from "react"
+import { fonte } from "../../../../public/js/fonte"
+import { setFonte } from "../../../../public/js/fonte"
 
 export default function BarraAcessibilidade() {
 
-    let cssvar = new CSSGlobalVariables()
+    //Como a barra de acessibilidade rederiza em várias páginas,
+    //vou usar o useEffect para verificar o cookie
 
-    let chaves = Object.keys(cssvar)
-    let valores = Object.values(cssVariaveis)
-
-    function handleContraste() {
-        chaves.forEach(chave => {
-            if (chave.includes("border")) {
-                cssvar[chave] = "#fff"
-            }
-
-            if (chave.includes("highlight")) {
-                cssvar[chave] = "#000"
-            }
-
-            if (chave.includes("text")) {
-                cssvar[chave] = "#fff"
-            }
-
-
-            if (chave.includes("background")) {
-                cssvar[chave] = "#000"
-            }
-
-            if (chave.includes("link")) {
-                cssvar[chave] = "#ffff00"
-            }
-
-            if (chave.includes("selected")) {
-                cssvar[chave] = "#0000ff"
-            }
-
-        });
-    }
-
-    function handleSemContraste() {
-        let i = 0;
-
-
-        chaves.forEach(c => {
-
-            cssvar[c] = valores[i]
-
-            i++
-
-        })
-
-
-    }
-
-
-    function fonteMais() {
-
-        if (fonte < 25) {
-            setFonte(fonte + 1)
-            document.documentElement.style.fontSize = fonte + "px"
+    useEffect(() => {
+        const ckContraste = Cookies.get('contraste')
+        //'documentElement' encontra o elemento raiz, ou seja, o <HTML>
+        if(ckContraste === "true"){
+            document.documentElement.classList.add('coresAcessibilidade')
+        }else{
+            document.documentElement.classList.remove('coresAcessibilidade')
         }
 
+}, [])
+
+    function constrate(){
+        document.documentElement.classList.add('coresAcessibilidade')
+        Cookies.set('contraste', true)
+    }
+
+    function semContraste(){
+        document.documentElement.classList.remove('coresAcessibilidade')
+        Cookies.set('contraste', false)
+    }
+
+    
+    function fonteMais() {
+
+        if (fonte < 24) {
+            setFonte(fonte + 2)
+            document.documentElement.style.fontSize = fonte + "px"
+        }
     }
 
     function fonteMenos() {
 
         if (fonte > 16) {
-            setFonte(fonte - 1)
+            setFonte(fonte - 2)
             document.documentElement.style.fontSize = fonte + "px"
         }
 
@@ -80,10 +53,10 @@ export default function BarraAcessibilidade() {
         <div className={styles.container}>
             <ul className={styles.lista}>
                 <li><Link className={styles.link} to="/acessibilidade">Acessibilidade</Link></li>
-                <li><button className={styles.botao} onClick={handleContraste}>Contraste</button></li>
-                <li><button className={styles.botao} onClick={handleSemContraste}>Sem contraste</button></li>
-                <li><button className={styles.botao} onClick={fonteMais} id="fontemais">A+</button></li>
-                <li><button className={styles.botao} onClick={fonteMenos} id="fontmenos">A-</button></li>
+                <li><button className={styles.botao} onClick={()=> constrate()}>Contraste</button></li>
+                <li><button className={styles.botao} onClick={()=> semContraste()}>Sem contraste</button></li>
+                <li><button className={styles.botao} onClick={()=> fonteMais()}>A+</button></li>
+                <li><button className={styles.botao} onClick={()=> fonteMenos()}>A-</button></li>
             </ul>
         </div>
     )
