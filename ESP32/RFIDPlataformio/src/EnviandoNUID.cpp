3964e2e019f5b4eec1408d84b7247e99d1967356
Carjooj <victor@4info.com.br>
2024-05-16 11:17:08 -0300

    Importando .ino para o plataformio

---
+++ ESP32/RFIDPlataformio/src/EnviandoNUID.cpp
@@ -0,0 +1,70 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+
+#define SS_PIN 21
+#define RST_PIN 22
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+MFRC522::MIFARE_Key key;
+
+byte nuidPICC[4];
+
+char strId[32] = "";
+
+char charId = 'g';
+char getId;
+
+void array_to_string(byte array[], unsigned int len, char buffer[]);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+
+}
+
+void loop() {
+  getId = Serial.read();
+
+  while (getId == charId) {
+    if (rfid.PICC_IsNewCardPresent()) {
+      if (rfid.PICC_ReadCardSerial()) {
+        for (byte i = 0; i < 4; i++) {
+          nuidPICC[i] = rfid.uid.uidByte[i];
+        }
+        array_to_string(rfid.uid.uidByte, 4, strId);
+        Serial.print(strId);
+        Serial.println();
+        getId = 'a';
+      }
+    }
+  }
+
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+}
+
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++ ) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+void array_to_string(byte array[], unsigned int len, char buffer[]) {
+  for (unsigned int i = 0; i < len; i++)
+  {
+    byte nib1 = (array[i] >> 4) & 0x0F;
+    byte nib2 = (array[i] >> 0) & 0x0F;
+    buffer[i * 2 + 0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+    buffer[i * 2 + 1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+  }
+  buffer[len * 2] = '\0';
+}
