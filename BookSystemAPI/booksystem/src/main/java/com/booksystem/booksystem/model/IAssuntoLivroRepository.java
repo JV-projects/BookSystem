05edfc0c3578966fefcd705be0fedc06c6efabd4
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-05-15 23:00:12 -0300

    Criação de classes Repository | Livro, Assunto, AssuntoLivro

---
+++ BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/IAssuntoLivroRepository.java
@@ -0,0 +1,7 @@
+package com.booksystem.booksystem.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAssuntoLivroRepository extends JpaRepository<AssuntoLivro, Long>{
+
+}
---
+++ BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/IAssuntoRepository.java
@@ -0,0 +1,7 @@
+package com.booksystem.booksystem.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAssuntoRepository extends JpaRepository<Assunto, Long>{
+    
+}   
