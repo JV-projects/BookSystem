5851439502d44158a4f62fbbadeca5d743e560ef
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-05-20 22:26:08 -0300

    Status | Criação da enum para status de livro e empréstimo.

--- BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/IAssuntoLivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.booksystem.booksystem.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAssuntoLivroRepository extends JpaRepository<AssuntoLivro, Long>{
-
-}
--- BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/IAssuntoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.booksystem.booksystem.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAssuntoRepository extends JpaRepository<Assunto, Long>{
-    
-}   
