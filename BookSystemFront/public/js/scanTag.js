0822cf809f09da2305e0818ecfaa3ad2dcaf24db
Carjooj <victor@4info.com.br>
2024-05-20 22:19:14 -0300

    scanTag.js | Script para escanear as Tags funcional

--- BookSystemFront/public/js/scanTag.js
+++ BookSystemFront/public/js/scanTag.js
@@ -1,19 +1,54 @@
-let port;
-let i = 0;
-let teste;
-let reader;
-const bufferSize = 10;
-let buffer = new ArrayBuffer(bufferSize)
-let buffer1 = new ArrayBuffer(bufferSize)
 let array = []
 
-export default async function abrirPorta(velocidade) {
+export async function abrirPorta(velocidade) {
     let porta = await navigator.serial.requestPort();
 
-    await porta.open({baudRate: velocidade})
+    await porta.open({ baudRate: velocidade })
 
     console.log(porta)
 
     return porta
 }
+
+export async function enviarSerial(porta) {
+    const encoder = new TextEncoder();
+    const writer = porta.writable.getWriter();
+    await writer.write(encoder.encode('g'));
+    writer.releaseLock();
+}
+
+export async function lerPorta(porta, set) {
+
+    enviarSerial(porta)
+
+    const textDecoder = new TextDecoderStream();
+    const readableStreamClosed = porta.readable.pipeTo(textDecoder.writable)
+    const leitor = textDecoder.readable.getReader();
+
+    while (true) {
+        const { value, done } = await leitor.read();
+        if (done) {
+            leitor.releaseLock();
+            break;
+        }
+        console.log(value)
+        guardarByte(value)
+        set(array)
+    }
+}
+
+function guardarByte(bytes) {
+    for (let i = 0; i < bytes.length; i++) {
+        array.push(bytes[i])
+    }
+
+    return array
+}
+
+export function arrayToString(array) {
+    array = array.toString()
+    array = array.replaceAll(',', "")
+    return array
+}
+
 
