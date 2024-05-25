ab4c501c52755bc16244f558c142522a7fabb24a
felipe-coletti <felipecoletti2@gmail.com>
2024-05-25 11:25:45 -0300

    Teste consulta livro

--- BookSystemAPI/booksystem/src/test/java/com/booksystem/booksystem/BooksystemApplicationTests.java
+++ BookSystemAPI/booksystem/src/test/java/com/booksystem/booksystem/BooksystemApplicationTests.java
@@ -3,11 +3,26 @@ package com.booksystem.booksystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.booksystem.booksystem.model.Livro;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.booksystem.booksystem.servico.LivroServico;
+import com.booksystem.booksystem.model.repository.ILivroRepository;
+
 @SpringBootTest
 class BooksystemApplicationTests {
+	@Autowired
+	private ILivroRepository livroRepository;
 
 	@Test
-	void contextLoads() {
+	void testAssunto() {
+		LivroServico servico = new LivroServico(livroRepository);
+
+		assert(!servico.consultarLivros().isEmpty())
 	}
 
 }
