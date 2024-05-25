399d0c53f1d9431de8ede81191c8f434821dcc66
Carjooj <victor@4info.com.br>
2024-05-25 11:00:44 -0300

    Atividade teste CI/CD

--- BookSystemAPI/booksystem/src/test/java/com/booksystem/booksystem/BooksystemApplicationTests.java
+++ BookSystemAPI/booksystem/src/test/java/com/booksystem/booksystem/BooksystemApplicationTests.java
@@ -1,13 +1,30 @@
 package com.booksystem.booksystem;
 
+import com.booksystem.booksystem.model.Assunto;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.booksystem.booksystem.servico.AssuntoServico;
+import com.booksystem.booksystem.model.repository.IAssuntoRepository;
 
 @SpringBootTest
 class BooksystemApplicationTests {
 
+	@Autowired
+	private IAssuntoRepository assuntoRepository;
+
 	@Test
-	void contextLoads() {
+	void testeCadastroAssunto() {
+
+
+		AssuntoServico as = new AssuntoServico(assuntoRepository);
+
+		Assunto assunto = new Assunto("Romance");
+
+		as.cadastrarAssunto(assunto);
+
+
 	}
 
 }
