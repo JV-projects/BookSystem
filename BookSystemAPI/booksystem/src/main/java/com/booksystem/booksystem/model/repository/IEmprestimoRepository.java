8d19ed10a8d99c92a2cc306aa344aaabf071fe6c
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-05-27 22:57:54 -0300

    Adicionando notação @Repository nas interfaces repository

--- BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/repository/IEmprestimoRepository.java
+++ BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/repository/IEmprestimoRepository.java
@@ -2,6 +2,8 @@ package com.booksystem.booksystem.model.repository;
 
 import com.booksystem.booksystem.model.Emprestimo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IEmprestimoRepository extends MongoRepository<Emprestimo, String>{
 }
--- BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/repository/ILivroRepository.java
+++ BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/repository/ILivroRepository.java
@@ -1,10 +1,16 @@
 package com.booksystem.booksystem.model.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.booksystem.booksystem.model.Livro;
 
+@Repository
 public interface ILivroRepository extends MongoRepository<Livro, String>{
-    List<Livro> findByIsbn(long isbn);
+    public List<Livro> findByIsbn(long isbn);
 }
