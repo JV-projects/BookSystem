215c8a381218741990cb07cba600040ebe540c80
Carjooj <victor@4info.com.br>
2024-05-31 12:44:00 -0300

    Classe de Controle APILivroController Adicionando Endpoints de pesquisa

--- BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/controller/APILivroController.java
+++ BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/controller/APILivroController.java
@@ -1,15 +1,11 @@
 package com.booksystem.booksystem.controller;
 
-import org.springframework.web.bind.annotation.*;
-
 import com.booksystem.booksystem.model.Livro;
 import com.booksystem.booksystem.servico.interfaces.ILivroServico;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -17,22 +13,50 @@ import org.springframework.http.ResponseEntity;
 public class APILivroController {
     Logger logger = LogManager.getLogger(this.getClass());
 
-    private ILivroServico livroServico;
+    private final ILivroServico livroServico;
 
-    public APILivroController(ILivroServico livroServico){
+    public APILivroController(ILivroServico livroServico) {
         this.livroServico = livroServico;
     }
 
     @GetMapping("livros")
-    public ResponseEntity<Object> consultarLivros(){
+    public ResponseEntity<Object> consultarLivros() {
         logger.info("api --> Consultar livro");
         return ResponseEntity.ok().body(livroServico.consultarLivros());
     }
 
-    @GetMapping(value = "livros", params = {"titulo", "ordem"})
-    public ResponseEntity<Object> consultaPorTitulo(@RequestParam String titulo, @RequestParam int ordem) {
+    @GetMapping(value = "livros", params = {"titulo", "filtro", "ordem"})
+    public ResponseEntity<Object> consultaPorTitulo(@RequestParam String titulo,
+                                                    @RequestParam(required = false, defaultValue = "0") int ano,
+                                                    @RequestParam(required = false) String filtro,
+                                                    @RequestParam int ordem) {
+
         logger.info("api --> Consulta por Titulo");
-        return ResponseEntity.ok().body(livroServico.consultarPorTitulo(titulo, ordem));
+        return ResponseEntity.ok().body(livroServico.consultarPorTitulo(titulo, ano, filtro, ordem));
+    }
+
+    @GetMapping(value = "livros", params = "isbn")
+    public ResponseEntity<Object> consultarPorIsbn(@RequestParam long isbn) {
+        logger.info("api --> Consulta por ISBN");
+        return ResponseEntity.ok(livroServico.consultarPorIsbn(isbn));
+    }
+
+    @GetMapping(value = "livros", params = {"autor", "filtro", "ordem"})
+    public ResponseEntity<Object> consultaPorAutor(@RequestParam String autor,
+                                                   @RequestParam(required = false, defaultValue = "0") int ano,
+                                                   @RequestParam(required = false) String filtro,
+                                                   @RequestParam int ordem) {
+        logger.info("api --> Consulta por autor");
+        return ResponseEntity.ok().body(livroServico.consultarPorAutor(autor, ano, filtro, ordem));
+    }
+
+    @GetMapping(value = "livros", params = {"editora", "filtro", "ordem"})
+    public ResponseEntity<Object> consultarPorEditora(@RequestParam String editora,
+                                                      @RequestParam(required = false, defaultValue = "0") int ano,
+                                                      @RequestParam(required = false) String filtro,
+                                                      @RequestParam int ordem) {
+        logger.info("api --> Consulta por editora");
+        return ResponseEntity.ok().body(livroServico.consultarPorEditora(editora, ano, filtro, ordem));
     }
 
     @PostMapping("livros")
@@ -42,7 +66,5 @@ public class APILivroController {
         return ResponseEntity.ok().body(livroServico.cadastrarLivro(livro));
     }
 
-
-    
 
 }
