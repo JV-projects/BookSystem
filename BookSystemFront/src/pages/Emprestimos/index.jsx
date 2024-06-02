bcf16c08b5c927ba5640d48eaa279642ac98154f
felipe-coletti <felipecoletti2@gmail.com>
2024-06-02 01:57:38 -0300

    Emprestimos | Aplicação do swr

--- BookSystemFront/src/pages/Emprestimos/index.jsx
+++ BookSystemFront/src/pages/Emprestimos/index.jsx
@@ -8,34 +8,14 @@ import Status from '../../components/global/Status'
 import TopoPagina from '../../components/global/TopoPagina'
 import Aside from '../../components/global/Aside'
 import DetalhesEmprestimo from '../../components/global/DetalhesEmprestimo'
+import useSWR from 'swr'
+import fetcher from '../../util/fetcher'
 
 export default function Emprestimos() {
-    const [aberto, setAberto] = useState(false)
-    
+    const { data, error, isLoading } = useSWR('booksystem/api/livros', fetcher)
 
-    const dados = [
-        {
-            "nome": "Jackeline Menezes",
-            "numeroTelefone": "(11) 98822-8833",
-            "dataEmprestimo": "21/04/2024",
-            "dataDevolucao": "21/05/2024",
-            "status": "Em atraso",
-        },
-        {
-            "nome": "Yudi Tamashiro",
-            "numeroTelefone": "(11) 4002-8922",
-            "dataEmprestimo": "21/04/2024",
-            "dataDevolucao": "21/05/2024",
-            "status": "Em andamento",
-        },
-        {
-            "nome": "Yudi Tamashiro",
-            "numeroTelefone": "(11) 4002-8922",
-            "dataEmprestimo": "21/04/2024",
-            "dataDevolucao": "21/05/2024",
-            "status": "Concluído",
-        }
-    ]
+    const [aberto, setAberto] = useState(false)
+    const [indiceSelecionado, setIndiceSelecionado] = useState(null)
 
     let pesquisar = [
         { valor: "titulo", texto: "Título" },
@@ -57,7 +37,6 @@ export default function Emprestimos() {
     return (
         <EstruturaPagina>
             <TopoPagina titulo="Empréstimos" subtitulo="Histórico"/>
-            <div className={styles.areaConteudo}>
                 <div className={styles.barraOpcoes}>
                     <div className={styles.containerPesquisa}>
                         <div className={styles.blocoPesquisa + " " + styles.areaPesquisa}>
@@ -72,32 +51,45 @@ export default function Emprestimos() {
                         </div>
                     </div>
                 </div>
-                <div className={styles.containerCartoes}>
-                    {dados.map((item, i) => (
-                        <div className={styles.cartao} key={i}>
-                            <p className={styles.paragrafo}>{item.nome}</p>
-                            <p className={styles.paragrafo + " " + styles.tel}>
-                                <span className="material-symbols-outlined">call</span>
-                                {item.numeroTelefone}
-                            </p>
-                            <div className={styles.areaTexto + " " + styles.data}>
-                                <h2 className={styles.tituloSecundario}>Data de retirada</h2>
-                                <p className={styles.paragrafo}>{item.dataEmprestimo}</p>
+                {error ? (
+                    <div className={styles.areaTextoCentralizado}>
+                        <p className={styles.paragrafo}>Ocorreu um erro.</p>
+                    </div>
+                ): isLoading ? (
+                    <div className={styles.areaTextoCentralizado}>
+                        <p className={styles.paragrafo}>Carregando...</p>
+                    </div>
+                ) : !data ? (
+                    <div className={styles.areaTextoCentralizado}>
+                        <p className={styles.paragrafo}>Nenhum dado encontrado.</p>
+                    </div>
+                ) : (
+                    <div className={styles.containerCartoes}>
+                        {data.map((item, i) => (
+                            <div className={styles.cartao} key={i}>
+                                <p className={styles.paragrafo}>{item.nome}</p>
+                                <p className={styles.paragrafo + " " + styles.tel}>
+                                    <span className="material-symbols-outlined">call</span>
+                                    {item.numeroTelefone}
+                                </p>
+                                <div className={styles.areaTexto + " " + styles.data}>
+                                    <h2 className={styles.tituloSecundario}>Data de retirada</h2>
+                                    <p className={styles.paragrafo}>{item.dataEmprestimo}</p>
+                                </div>
+                                <div className={styles.areaTexto + " " + styles.data}>
+                                    <h2 className={styles.tituloSecundario}>Data de devolução</h2>
+                                    <p className={styles.paragrafo}>{item.dataDevolucao}</p>
+                                </div>
+                                <Status status={item.status} />
+                                <Button onClick={() => {setIndiceSelecionado(i), setAberto(true)}}>
+                                    <span className="material-symbols-outlined">info</span>
+                                </Button>
                             </div>
-                            <div className={styles.areaTexto + " " + styles.data}>
-                                <h2 className={styles.tituloSecundario}>Data de devolução</h2>
-                                <p className={styles.paragrafo}>{item.dataDevolucao}</p>
-                            </div>
-                            <Status status={item.status} />
-                            <Button onClick={() => setAberto(true)}>
-                                <span className="material-symbols-outlined">info</span>
-                            </Button>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <Aside aberto={aberto} fechar={() => setAberto(false)} titulo={'Detalhes do empréstimo'}>
-                <DetalhesEmprestimo/>
+                        ))}
+                    </div>
+                )}
+            <Aside aberto={aberto} fechar={() => {setIndiceSelecionado(null), setAberto(false)}} titulo={'Detalhes do empréstimo'}>
+                <DetalhesEmprestimo id={data && data[indiceSelecionado].id}/>
             </Aside>
         </EstruturaPagina>
     )
