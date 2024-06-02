d55f6172925b2eda1d64edaed84bd88e069c2b60
felipe-coletti <felipecoletti2@gmail.com>
2024-06-02 01:56:04 -0300

    Modal | Remoção de atributo desnecessário

--- BookSystemFront/src/components/global/Modal/index.jsx
+++ BookSystemFront/src/components/global/Modal/index.jsx
@@ -1,12 +1,12 @@
 import styles from './styles.module.css'
 import Button from '../Button'
 
-export default function Modal({ aberto, fechar, backdropClose, titulo, children }) {
+export default function Modal({ aberto, fechar, titulo, children }) {
     if (!aberto) return null
 
     return (
         <div className={styles.container}>
-            <div className={styles.backdrop} onClick={backdropClose}></div>
+            <div className={styles.backdrop} onClick={fechar}></div>
             <div className={styles.areaConteudo}>
                 <div className={styles.areaTopo}>
                     <h1 className={styles.titulo}>{titulo}</h1>
