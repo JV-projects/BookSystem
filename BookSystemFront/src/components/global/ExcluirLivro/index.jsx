0d200ec7822ac4281c26e3c69f518a812ec4067a
felipe-coletti <felipecoletti2@gmail.com>
2024-06-02 01:55:27 -0300

    ExcluirLivro | Aplicação do swr

--- BookSystemFront/src/components/global/ExcluirLivro/index.jsx
+++ BookSystemFront/src/components/global/ExcluirLivro/index.jsx
@@ -1,13 +1,28 @@
 import styles from './styles.module.css'
 import Button from '../Button'
+import useSWR from 'swr'
+import fetcher from '../../../util/fetcher'
 
-export default function ExcluirLivro() {
+export default function ExcluirLivro({ id }) {
+    const { data, error, isLoading } = useSWR(`/booksystem/api/livros/${id}`, fetcher)
+
+    if (error) return (
+        <div className={styles.areaTextoCentralizado}>
+            <p className={styles.paragrafo}>Ocorreu um erro.</p>
+        </div>
+    )
+    if (isLoading) return (
+        <div className={styles.areaTextoCentralizado}>
+            <p className={styles.paragrafo}>Carregando...</p>
+        </div>
+    )
+
     return (
         <div className={styles.container}>
             <p className={styles.paragrafo}>Tem certeza de que deseja excluir esse livro?</p>
             <div className={styles.cartao}>
-                <h1 className={styles.titulo}>Nome do livro</h1>
-                <p className={styles.destaque}>Autor do livro</p>
+                <h1 className={styles.titulo}>{data.titulo}</h1>
+                <p className={styles.destaque}>{data.autor}</p>
             </div>
             <div className={styles.areaBotoes}>
                 <Button tipoBotao={'terciarioCancela'}>Cancelar</Button>
