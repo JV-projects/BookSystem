7a415478c7cc3e3e807f1df9771a38b067b652de
felipe-coletti <felipecoletti2@gmail.com>
2024-06-02 01:58:05 -0300

    Home | Aplicação do swr

--- BookSystemFront/src/pages/Home/index.jsx
+++ BookSystemFront/src/pages/Home/index.jsx
@@ -11,47 +11,16 @@ import Modal from '../../components/global/Modal'
 import DetalhesLivro from '../../components/global/DetalhesLivro'
 import ExcluirLivro from '../../components/global/ExcluirLivro'
 import Assunto from '../../components/global/Assunto'
+import useSWR from 'swr'
+import fetcher from '../../util/fetcher'
 
 export default function Home() {
+    const { data, error, isLoading } = useSWR('booksystem/api/livros', fetcher)
+    
     const [pesquisa, setPesquisa] = useState("")
-    const [indiceSelecionado, setIndiceSelecionado] = useState(null)
+    const [indiceSelecionado, setIndiceSelecionado] = useState(0)
     const [modalSelecionado, setModalSelecionado] = useState(null)
-
-
-    const dados = [
-        {
-            "id": 1,
-            "titulo": "Orgulho e Preconceito",
-            "autor": "Jane Austen",
-            "editora": "Martin Claret",
-            "ano": "2012",
-            "assuntos": [
-                {
-                    "id": 1,
-                    "assunto": "Literatura estrangeira"
-                },
-                {
-                    "id": 2,
-                    "assunto": "Romance"
-                }
-            ],
-            "status": "Disponível",
-        },
-        {
-            "id": 2,
-            "titulo": "Java®: Como Programar",
-            "autor": "Paul Deitel",
-            "editora": "Pearson Universidades",
-            "ano": "2016",
-            "assuntos": [
-                {
-                    "id": 3,
-                    "assunto": "Programação"
-                }
-            ],
-            "status": "Indisponível",
-        }
-    ]
+    const [lista, setLista] = useState(data)
 
     let pesquisar = [
         { valor: "titulo", texto: "Título" },
@@ -67,23 +36,20 @@ export default function Home() {
         { valor: "editora", texto: "Editora" }
     ]
 
-
-    const [lista, setLista] = useState(dados)
-
     const handlePesquisar = () => {
         pesquisa.length ? setLista(
-            dados.filter(item =>
+            data.filter(item =>
                 item.titulo.toLowerCase().includes(pesquisa.toLowerCase()) ||
                 item.autor.toLowerCase().includes(pesquisa.toLowerCase()) ||
                 item.editora.toLowerCase().includes(pesquisa.toLowerCase()) ||
                 item.ano.toLowerCase().includes(pesquisa.toLowerCase())
             )
-        ) : setLista(dados)
+        ) : setLista(data)
     }
 
 
     const handleSelecionar = (indice) => {
-        if (indiceSelecionado == indice) {
+        if (indiceSelecionado === indice) {
             setIndiceSelecionado(null)
         } else {
             setIndiceSelecionado(indice)
@@ -92,16 +58,13 @@ export default function Home() {
 
     return (
         <EstruturaPagina>
-
             <TopoPagina titulo="Gerenciamento" />
-
             <div className={styles.barraOpcoes}>
                 <div className={styles.containerPesquisa}>
                     <div className={styles.blocoPesquisa + " " + styles.areaPesquisa}>
                         <Input className={styles.barraPesquisa} placeholder='Pesquisar' />
                         <Button tipoBotao="primario" onClick={handlePesquisar}>
                             <span className="material-symbols-outlined">search</span>
-
                         </Button>
                     </div>
                     <div className={styles.blocoFiltro}>
@@ -112,14 +75,11 @@ export default function Home() {
                 </div>  
                 <span className={styles.linha} />
                 <div className={styles.containerBotoes}>
-
                     <Link to="/novoemprestimo">
                         <Button tipoBotao="primario">
                             <p>Novo empréstimo</p>
                         </Button>
                     </Link>
-
-
                     <div className={styles.areaBotoes}>
                         <Link to="/criar">
                             <Button icone="add" tipoBotao="primario">
@@ -127,89 +87,75 @@ export default function Home() {
                             </Button>
                         </Link>
                         <Button tipoBotao="secundario" icone="info" onClick={() => setModalSelecionado('detalhes')} disabled={indiceSelecionado === null}>
-                        <p className={styles.action}>Detalhes</p>
+                            <p className={styles.action}>Detalhes</p>
                         </Button>
                         <Link to="/editar">
                             <Button tipoBotao="secundario" icone="edit_square" disabled={indiceSelecionado === null}>
-                            <p className={styles.action}>Editar</p>
+                                <p className={styles.action}>Editar</p>
                             </Button>
                         </Link>
                         <Button tipoBotao="secundario" icone="delete" onClick={() => setModalSelecionado('excluir')} disabled={indiceSelecionado === null}>
-                        <p className={styles.action}>Excluir</p>
+                            <p className={styles.action}>Excluir</p>
                         </Button>
                     </div>
-
                 </div>
             </div>
-            <div className={styles.containerCartoes}>
-                {lista.map((item, i) => (
-
-                    <div className={indiceSelecionado == i ? styles.cartaoSelecionado : styles.cartaoNaoSelecionado} onClick={() => handleSelecionar(i)} key={i}>
-
-                        <div className={styles.imagemCartao}>
-                            <img alt={`Foto do livro ${item.titulo}`} />
-                        </div>
-                        <div className={styles.areaConteudoCartao}>
-                            <div className={styles.areaTexto}>
-                                <h2 className={styles.tituloSecundario}>{item.titulo}</h2>
+            {error ? (
+                <div className={styles.areaTextoCentralizado}>
+                    <p className={styles.paragrafo}>Ocorreu um erro.</p>
+                </div>
+            ): isLoading ? (
+                <div className={styles.areaTextoCentralizado}>
+                    <p className={styles.paragrafo}>Carregando...</p>
+                </div>
+            ) : !data ? (
+                <div className={styles.areaTextoCentralizado}>
+                    <p className={styles.paragrafo}>Nenhum dado encontrado.</p>
+                </div>
+            ) : (
+                <div className={styles.containerCartoes}>
+                    {lista.map((item, i) => (
+                        <div className={indiceSelecionado == i ? styles.cartaoSelecionado : styles.cartaoNaoSelecionado} onClick={() => handleSelecionar(i)} key={i}>
+                            <div className={styles.imagemCartao}>
+                                <img alt={`Foto do livro ${item.titulo}`} />
+                            </div>
+                            <div className={styles.areaConteudoCartao}>
+                                <div className={styles.areaTexto}>
+                                    <h2 className={styles.tituloSecundario}>{item.titulo}</h2>
+                                    <div className={styles.areaBotoes}>
+                                        <div className={styles.grupoTexto}>
+                                            <p className={styles.paragrafo}>Autor:</p>
+                                            <span className={styles.destaque}>{item.autor}</span>
+                                            <p className={styles.paragrafo}><span className={styles.linhaHorizontal} /></p>
+                                        </div>
+                                        <div className={styles.grupoTexto}>
+                                            <p className={styles.paragrafo}>Editora:</p>
+                                            <span className={styles.destaque}>{item.editora}</span>
+                                            <p className={styles.paragrafo}><span className={styles.linhaHorizontal} /></p>
+                                        </div>
+                                        <div className={styles.grupoTexto}>
+                                            <p className={styles.paragrafo}>Ano:</p>
+                                            <span className={styles.destaque}>{item.ano}</span>
+                                        </div>
+                                    </div>
+                                </div>
                                 <div className={styles.areaBotoes}>
-
-                                    <div className={styles.grupoTexto}>
-                                        <p className={styles.paragrafo}>Autor:</p>
-                                        <span className={styles.destaque}>{item.autor}</span>
-
-                                        <p className={styles.paragrafo}><span className={styles.linhaHorizontal} /></p>
-                                    </div>
-
-                                    <div className={styles.grupoTexto}>
-                                        <p className={styles.paragrafo}>Editora:</p>
-                                        <span className={styles.destaque}>{item.editora}</span>
-
-                                        <p className={styles.paragrafo}><span className={styles.linhaHorizontal} /></p>
-                                    </div>
-
-                                    <div className={styles.grupoTexto}>
-                                        <p className={styles.paragrafo}>Ano:</p>
-                                        <span className={styles.destaque}>{item.ano}</span>
-                                    </div>
-
-
+                                    {item.assuntos.map((assunto, i) => (
+                                        <Assunto key={i}><p>{assunto.assunto}</p></Assunto>
+                                    ))}
                                 </div>
                             </div>
-                            <div className={styles.areaBotoes}>
-                                {item.assuntos.map((assunto, i) => (
-                                    <Assunto key={i}><p>{assunto.assunto}</p></Assunto>
-                                ))}
-                            </div>
+                            <Status status={item.status} />
                         </div>
-                        <Status status={item.status} />
-                    </div>
-                ))}
-            </div>
-            <Modal aberto={modalSelecionado === 'detalhes'} fechar={setModalSelecionado} 
-            backdropClose={setModalSelecionado} titulo={'Detalhes do livro'}>
-                <DetalhesLivro />
+                    ))}
+                </div>
+            )}
+            <Modal aberto={modalSelecionado === 'detalhes'} fechar={() => setModalSelecionado(null)} titulo={'Detalhes do livro'}>
+                <DetalhesLivro id={data && lista[indiceSelecionado].id} />
             </Modal>
-            <Modal aberto={modalSelecionado === 'excluir'} fechar={setModalSelecionado} 
-            backdropClose={setModalSelecionado} titulo={'Excluir livro'}>
-                <ExcluirLivro />
+            <Modal aberto={modalSelecionado === 'excluir'} fechar={() => setModalSelecionado(null)} titulo={'Excluir livro'}>
+                <ExcluirLivro id={data && lista[indiceSelecionado].id} />
             </Modal>
-
         </EstruturaPagina>
     )
 }
-
-/*
- const handleSelecionar = (indice) => {
-        if (indiceSelecionado == indice) {
-            setSelecionado(!selecionado)
-        }
-        setIndiceSelecionado(indice)
-    } // <div className={selecionado && indiceSelecionado == i ? 
-    styles.cartaoSelecionado : styles.cartaoNaoSelecionado} onClick={() => handleSelecionar(i)} key={i}>
-
-    
-
-
-
-*/
