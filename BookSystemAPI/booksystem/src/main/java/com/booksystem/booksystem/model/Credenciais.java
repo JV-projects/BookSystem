ea953ace9c5ce7115e513806a5f2bf9f93fcdc9a
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-06-03 23:29:58 -0300

    Credenciais | Refatorando métodos da UserDetails

--- BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/Credenciais.java
+++ BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/model/Credenciais.java
@@ -81,21 +81,21 @@ public class Credenciais implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
