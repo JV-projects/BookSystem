6fde5b13a7db1e72c208b222c799524b26aa5d3c
felipe-coletti <felipecoletti2@gmail.com>
2024-06-03 22:40:30 -0300

    assuntosPredefinidos | Separação dos assuntos predefinidos para fácil reutilização

---
+++ BookSystemFront/src/util/assuntosPredefinidos.jsx
@@ -0,0 +1,16 @@
+const assuntosPredefinidos = [
+    {
+        valor: 'Literatura',
+        texto: 'Literatura'
+    },
+    {
+        valor: 'Romance',
+        texto: 'Romance'
+    },
+    {
+        valor: 'Programação',
+        texto: 'Programação'
+    }
+]
+
+export default assuntosPredefinidos
