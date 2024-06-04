0c1c9437b84b5e3bf718aaf509a8c31cb1d3d85c
felipe-coletti <felipecoletti2@gmail.com>
2024-06-03 22:32:39 -0300

    DetalhesEmprestimo | Aplicação do swr para integração com o back end

--- BookSystemFront/src/components/global/DetalhesEmprestimo/index.jsx
+++ BookSystemFront/src/components/global/DetalhesEmprestimo/index.jsx
@@ -1,17 +1,21 @@
 import styles from './styles.module.css'
 import Button from '../Button'
+import apiUrl from '../../../util/apiUrl'
 
-export default function DetalhesLivro() {
-    const dados = [
-        {
-            titulo: "Orgulho e Preconceito",
-            autor: "Jane Austen"
-        },
-        {
-            titulo: "Java®: Como Programar",
-            autor: "Paul Deitel"
-        }
-    ]
+export default function DetalhesLivro({ id }) {
+    const { emprestimo, error, isLoading } = useSWR(`${apiUrl}/emprestimos/${id}`, fetcher)
+    const { usuario, userError, userIsLoading } = useSWR(`${apiUrl}/usuarios/${id}`, fetcher)
+
+    if (error) return (
+        <div className={styles.areaTextoCentralizado}>
+            <p className={styles.paragrafo}>Ocorreu um erro.</p>
+        </div>
+    )
+    if (isLoading) return (
+        <div className={styles.areaTextoCentralizado}>
+            <p className={styles.paragrafo}>Carregando...</p>
+        </div>
+    )
 
     return (
         <div className={styles.container}>
@@ -19,24 +23,32 @@ export default function DetalhesLivro() {
                 <p className={styles.paragrafo}>Usuário</p>
             </div>
             <div className={styles.areaTexto}>
-                <p className={styles.paragrafo}>Nome</p>
-                <div className={styles.areaDados}>
-                    <p className={styles.paragrafo}>
-                        <span className="material-symbols-outlined">mail</span>
-                        Endereço de e-mail
-                    </p>
-                    <p className={styles.paragrafo}>
-                        <span className="material-symbols-outlined">call</span>
-                        Número de telefone
-                    </p>
-                </div>
+                {userError ? (
+                    <p className={styles.paragrafo}>Ocorreu um erro ao buscar os dados do usuário.</p>
+                ) : userIsLoading ? (
+                    <p className={styles.paragrafo}>Carregando...</p>
+                ) : (
+                    <>
+                        <p className={styles.paragrafo}>{usuario.nome}</p>
+                        <div className={styles.areaDados}>
+                            <p className={styles.paragrafo}>
+                                <span className="material-symbols-outlined">mail</span>
+                                {usuario.email}
+                            </p>
+                            <p className={styles.paragrafo}>
+                                <span className="material-symbols-outlined">call</span>
+                                {usuario.telefone}
+                            </p>
+                        </div>
+                    </>
+                )}
             </div>
             <div className={styles.bloco}>
                 <p className={styles.paragrafo}>Itens</p>
                 <p className={styles.paragrafo}>0 itens</p>
             </div>
             <ul className={styles.lista}>
-                {dados.map((livro, i) => (
+                {emprestimo.itensEmprestimo.map((livro, i) => (
                     <li className={styles.cartao} key={i}>
                         <h1 className={styles.titulo}>{livro.titulo}</h1>
                         <p className={styles.destaque}>{livro.autor}</p>
@@ -46,16 +58,16 @@ export default function DetalhesLivro() {
             <div className={styles.bloco}>
                 <div className={styles.areaTexto}>
                     <h1 className={styles.titulo}>Data de retirada</h1>
-                    <p className={styles.paragrafo}>00/00/0000</p>
+                    <p className={styles.paragrafo}>{emprestimo.dataRetirada}</p>
                 </div>
                 <span className={styles.linhaVertical} />
                 <div className={styles.areaTexto + " " + styles.alinhamentoDireita}>
                     <h1 className={styles.titulo}>Data de devolução</h1>
-                    <p className={styles.paragrafo}>00/00/0000</p>
+                    <p className={styles.paragrafo}>{emprestimo.dataDevolucao}</p>
                 </div>
             </div>
-            <Button tipoBotao={'terciarioConfirma'}>Confirmar</Button>
-            <Button tipoBotao={'terciarioCancela'}>Cancelar</Button>
+            <Button tipoBotao={'terciarioConfirma'}>Renovar</Button>
+            <Button tipoBotao={'terciarioCancela'}>Encerrar</Button>
         </div>
     )
 }
