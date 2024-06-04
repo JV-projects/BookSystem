0b62d820da09b05dea53ff8944940d1eb25c20ff
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-06-03 23:34:29 -0300

    APIUsuarioController | Endpoints Login e Registro

--- BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/controller/APIUsuarioController.java
+++ BookSystemAPI/booksystem/src/main/java/com/booksystem/booksystem/controller/APIUsuarioController.java
@@ -1,5 +1,7 @@
 package com.booksystem.booksystem.controller;
 
+import com.booksystem.booksystem.config.seguranca.SecurityConfigurations;
+import com.booksystem.booksystem.config.seguranca.TokenService;
 import com.booksystem.booksystem.model.Credenciais;
 import com.booksystem.booksystem.repository.IPerfilFuncionarioRepository;
 import org.apache.coyote.Response;
@@ -10,25 +12,50 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.booksystem.booksystem.repository.IPerfilUsuarioRepository;
 import com.booksystem.booksystem.repository.ICredenciaisRepository;
 
 @RestController
-@RequestMapping("booksystem/api/")
+@RequestMapping("booksystem/auth/")
 public class APIUsuarioController {
     Logger logger = LogManager.getLogger(this.getClass());
 
     @Autowired
     AuthenticationManager authenticationManager;
 
-    @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody Credenciais credenciais){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(credenciais.getUsername(), credenciais.getPassword());
-        var auth = authenticationManager.authenticate(usernamePassword);
+    @Autowired
+    ICredenciaisRepository credenciaisRepository;
 
-        return ResponseEntity.status(HttpStatus.OK).body("Autenticado");
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody CredenciaisDTO credenciaisDTO){
+        var usernameSenha = new UsernamePasswordAuthenticationToken(credenciaisDTO.username(), credenciaisDTO.senha());
+        var auth = this.authenticationManager.authenticate(usernameSenha);
+
+        var token = tokenService.gerarToken((Credenciais) auth.getPrincipal());
+
+        return ResponseEntity.status(HttpStatus.OK).body(token);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> registrar(@RequestBody RegistrarDTO registrarDTO){
+
+        if(credenciaisRepository.findByUsername(registrarDTO.username()).isPresent())
+            return ResponseEntity.badRequest().build();
+
+        String senhaEncrypted = new BCryptPasswordEncoder().encode(registrarDTO.senha());
+        Credenciais credenciais = new Credenciais(registrarDTO.username(), senhaEncrypted, registrarDTO.role());
+
+        credenciaisRepository.insert(credenciais);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Conta criada");
     }
 
 
