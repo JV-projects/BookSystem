a301c3e4e38a232f1263e341e75bc22cb3515015
felipe-coletti <felipecoletti2@gmail.com>
2024-06-03 22:28:53 -0300

    Assunto | Adição da função do botão de fechar

--- BookSystemFront/src/components/global/Assunto/index.jsx
+++ BookSystemFront/src/components/global/Assunto/index.jsx
@@ -1,10 +1,10 @@
 import styles from './styles.module.css'
 
-export default function Assunto({ fechavel=false, children, ...rest }){
+export default function Assunto({ children, fechavel=false, fechar, ...rest }){
     return(
         <p className={styles.assunto} {...rest}>
             {children}
-            {fechavel && <button className={styles.botao}>
+            {fechavel && <button className={styles.botao} type='button' onClick={fechar}>
                 <span className="material-symbols-outlined">close</span>
             </button>}
         </p>
