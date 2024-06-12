7468118af16a4843557b9bebbad040ab19352d4b
Carjooj <victor@4info.com.br>
2024-06-12 08:00:35 -0300

    Combinando métodos do ESP32

--- ESP32/RFIDPlataformio/src/EnviandoNUID.cpp
+++ ESP32/RFIDPlataformio/src/EnviandoNUID.cpp
@@ -4,10 +4,14 @@
 
 #define SS_PIN 21
 #define RST_PIN 22
+#define TOUCH_PIN 2
+#define BUZZER_PIN 4
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 
 MFRC522::MIFARE_Key key;
+
+byte uidAuth[4] = {0xA3, 0xA7, 0x3F, 0x25};
 
 byte nuidPICC[4];
 
@@ -16,9 +20,43 @@ char strId[32] = "";
 char charId = 'g';
 char getId;
 
+bool flag = true;
+
+const long interval = 3000;
+
 void array_to_string(byte array[], unsigned int len, char buffer[]);
+void printHex(byte *buffer, byte bufferSize);
+void rfidBegin();
+void rfidRead(char startRead);
+void rfidAuth();
 
 void setup() {
+  rfidBegin();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+}
+
+void loop() {
+  getId = Serial.read();
+  int getTouch = touchRead(15);
+
+  if (getTouch < 15) {
+    flag = !flag;
+  }
+
+  if (flag == true) {
+    digitalWrite(BUILTIN_LED, 1);
+    rfidRead(getId);
+  } else {
+    digitalWrite(BUILTIN_LED, 0);
+    rfidAuth();
+  }
+  
+}
+
+void rfidBegin() {
   Serial.begin(9600);
   SPI.begin();
   rfid.PCD_Init();
@@ -26,14 +64,10 @@ void setup() {
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
-
-
 }
 
-void loop() {
-  getId = Serial.read();
-
-  while (getId == charId) {
+void rfidRead(char startRead) {
+  while (startRead == charId) {
     if (rfid.PICC_IsNewCardPresent()) {
       if (rfid.PICC_ReadCardSerial()) {
         for (byte i = 0; i < 4; i++) {
@@ -42,13 +76,36 @@ void loop() {
         array_to_string(rfid.uid.uidByte, 4, strId);
         Serial.print(strId);
         Serial.println();
-        getId = 'a';
+        startRead = 'a';
       }
     }
   }
 
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
+}
+
+void rfidAuth() {
+  if (rfid.PICC_IsNewCardPresent()) {
+    if (rfid.PICC_ReadCardSerial()) {
+      if (rfid.uid.uidByte[0] == uidAuth[0] &&
+          rfid.uid.uidByte[1] == uidAuth[1] &&
+          rfid.uid.uidByte[2] == uidAuth[2] &&
+          rfid.uid.uidByte[3] == uidAuth[3] ) {
+        Serial.println("Acesso autorizado");
+      } else {
+        Serial.println("Acesso nao autorizado");
+          digitalWrite(BUZZER_PIN, 1);
+          digitalWrite(BUZZER_PIN, 0);
+
+          
+          
+      }
+      for (byte i = 0; i < 4; i++) {
+        nuidPICC[i] = rfid.uid.uidByte[i];
+      }
+    }
+  }
 }
 
 void printHex(byte *buffer, byte bufferSize) {
