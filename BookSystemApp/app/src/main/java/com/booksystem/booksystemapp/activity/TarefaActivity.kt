f42dca4a9f317f995fb6688b5302df80fe323b14
Carjooj <victor@4info.com.br>
2024-06-18 10:15:09 -0300

    Refatorando TarefaActivity para somente exibir as tarefas adicionadas

--- BookSystemApp/app/src/main/java/com/booksystem/booksystemapp/activity/TarefaActivity.kt
+++ BookSystemApp/app/src/main/java/com/booksystem/booksystemapp/activity/TarefaActivity.kt
@@ -1,14 +1,14 @@
 package com.booksystem.booksystemapp.activity
 
-import android.graphics.Color
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.booksystem.booksystemapp.TarefaAdapter
 import com.booksystem.booksystemapp.databinding.TarefasLayoutBinding
 import com.booksystem.booksystemapp.model.Tarefa
 import com.booksystem.booksystemapp.repository.TarefaRepository
-import com.google.android.material.snackbar.Snackbar
 
 class TarefaActivity : AppCompatActivity() {
 
@@ -27,6 +27,11 @@ class TarefaActivity : AppCompatActivity() {
         val adapter = TarefaAdapter(listaTarefas)
         binding.rcvTarefas.adapter = adapter
 
+        binding.btnCriarTarefa.setOnClickListener {
+            val intent = Intent(this, FormularioTarefaActivity::class.java)
+            startActivity(intent)
+        }
+
 
     }
 
@@ -40,36 +45,5 @@ class TarefaActivity : AppCompatActivity() {
                 binding.rcvTarefas.adapter?.notifyDataSetChanged()
             }
         }, {})
-
-        binding.btnAddTarefa.setOnClickListener { view ->
-            val edtTarefa = binding.edtTarefa
-            val tarefa = Tarefa(id = "", descricao = edtTarefa.text.toString())
-
-            repository.salvarTarefa(tarefa, {response ->
-                if (response.code == 200) {
-                    val snackbar = Snackbar.make(view, "Tarefa adicionada", Snackbar.LENGTH_SHORT)
-                    snackbar.setBackgroundTint(Color.DKGRAY)
-                    snackbar.show()
-                    repository.carregarTarefas({ response, tarefas ->
-                        this@TarefaActivity.runOnUiThread {
-                            listaTarefas.clear()
-                            listaTarefas.addAll(tarefas)
-                            binding.rcvTarefas.adapter?.notifyDataSetChanged()
-                        }
-                    }, {})
-
-                } else {
-                    val snackbar = Snackbar.make(view,
-                        "Não foi possível adicionar tarefa. Tente novamente", Snackbar.LENGTH_SHORT)
-                    snackbar.setBackgroundTint(Color.DKGRAY)
-                    snackbar.show()
-                }
-            }, { exception ->
-                val snackbar = Snackbar.make(view,
-                    "Falha: ${exception.localizedMessage}", Snackbar.LENGTH_SHORT)
-                snackbar.setBackgroundTint(Color.DKGRAY)
-                snackbar.show()
-            })
         }
-    }
 }
