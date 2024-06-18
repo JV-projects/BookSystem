f24bebe198184c47738c1e2832c2247d6525deb1
Carjooj <victor@4info.com.br>
2024-06-18 10:14:26 -0300

    Adicionando mais campos a Model Tarefa

--- BookSystemApp/app/src/main/java/com/booksystem/booksystemapp/model/Tarefa.kt
+++ BookSystemApp/app/src/main/java/com/booksystem/booksystemapp/model/Tarefa.kt
@@ -2,5 +2,7 @@ package com.booksystem.booksystemapp.model
 
 data class Tarefa(
     var id: String,
-    val descricao: String) {
+    val descricao: String,
+    val prioridade: String,
+    val marcador: String) {
 }
