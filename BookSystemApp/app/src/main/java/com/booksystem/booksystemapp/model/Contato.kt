c2e8669a279f0cb23578a2b23a90fe941a26d1e6
Carjooj <victor@4info.com.br>
2024-06-18 11:20:08 -0300

    Criando classe de modelo Contato

---
+++ BookSystemApp/app/src/main/java/com/booksystem/booksystemapp/model/Contato.kt
@@ -0,0 +1,8 @@
+package com.booksystem.booksystemapp.model
+
+data class Contato(
+    var id: String, val nome: String,
+    val email: String,
+    val telefone: String,
+    val grupo: String) {
+}
